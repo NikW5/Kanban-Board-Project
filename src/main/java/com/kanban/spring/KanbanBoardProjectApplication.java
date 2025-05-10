bc07aa5ffb6f560fdf2072964827ae05ce5262f9
NikW5 <86915684+NikW5@users.noreply.github.com>
2025-05-10 20:35:12 +0200

    thymeleaf eingebunden

--- src/main/java/com/kanban/spring/KanbanBoardProjectApplication.java
+++ src/main/java/com/kanban/spring/KanbanBoardProjectApplication.java
@@ -2,18 +2,10 @@ package com.kanban.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-public class KanbanBoardProjectApplication {
 
-	// index page Endung von url
-	@GetMapping("/start")
-	public String sayHello() {
-		return "Hello World!";
-	}
+public class KanbanBoardProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanbanBoardProjectApplication.class, args);
